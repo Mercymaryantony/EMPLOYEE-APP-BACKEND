9a50b0cd58b0fa9540e14df6c0ace8aba9b31d37
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-11 16:15:01 +0530

    changes made

--- app.js
+++ app.js
@@ -2,19 +2,34 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const app = express()
-const employee =require("./models/employee")
+const {employeemodel} =require("./models/employee")
 app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://mercy1112:mercy1112@cluster0.8x8j3ya.mongodb.net/employeeDB?retryWrites=true&w=majority&appName=Cluster0")
 
-app.listen(8080,()=>{
+app.listen(8081,()=>{
     console.log("server started")
 })
 
 app.get("/view",(req,res)=>{
-    res.send("VIEW")
+    employeemodel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.send("error")
+        }
+    )
+   
 })
 
 app.post("/add",(req,res)=>{
-    res.send("ADD")
+    let input = req.body
+    let employee = new employeemodel(input)
+    console.log(employee)
+    employee.save()
+    res.json({"status":"ADD"})
 })
 
 app.post("/delete",(req,res)=>{
--- models/employee.js
+++ models/employee.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose")
 const schema = mongoose.Schema(
     {
-        "NAME":String,
-                "CODE":String,
-                "ADD":String
+            "NAME":{type:String ,required:true},
+            "CODE":{type:String,required:true},
+            "ADD":{type:String,required:true},
+            "dob":String,
+            "img":String,
+            "pass":String,
+            "cpass":String
     }
 )
 let employeemodel =mongoose.model("employee",schema);
-module.export=(employeemodel)
+module.exports={employeemodel}
